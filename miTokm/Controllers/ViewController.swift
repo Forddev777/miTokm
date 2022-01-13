5bb4ea01c3ddc2de50b0ac07549a5fd315b1b906
Forddev777 <taothongdev@gmail.com>
2022-01-13 22:34:50 +0700

    first

--- miTokm/Controllers/ViewController.swift
+++ miTokm/Controllers/ViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Header_TItle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Header_TItle.backgroundColor = .blue
+        Header_TItle.textColor = .white
+      
+        
+      
+        
     }
+    
+    
+    
+    
+    
 
 
 }
